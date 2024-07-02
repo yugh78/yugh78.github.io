bca78d5675cab89186a9ce9c6ec0afc0c52514f9
Katy248 <petrovanton247@gmail.com>
2024-07-02 13:14:48 +0300

    Move summary to its own component

---
+++ src/components/SummarySection.vue
@@ -0,0 +1,23 @@
+<script setup>
+import SummaryCard from './SummaryCard.vue'
+import me from '../images/me.png'
+import me_and_who from '../images/me_and_who.png'
+const SUMMARIES = [
+  {
+    text: 'Cтудент первого курса по направлению программная инженерия ТУ им. А.А. Леонова.',
+    img: me
+  },
+  {
+    text: 'Занимаюсь каратэ Киокушинкай 7 лет. Так что могу и... на шпагат сесть.',
+    img: me_and_who
+  }
+]
+</script>
+<template>
+  <section>
+    <h2>Кратко <span class="letter-highlight">о</span>бо мне</h2>
+    <div class="card-container">
+      <SummaryCard v-for="sum in SUMMARIES" :key="sum" :img="sum.img" :text="sum.text" />
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import SummaryCard from '../components/SummaryCard.vue'
 import SkillsSection from '../components/SkillsSection.vue'
+import SummarySection from '../components/SummarySection.vue'
 </script>
 <template>
   <!-- Page header -->
@@ -10,27 +10,7 @@ import SkillsSection from '../components/SkillsSection.vue'
       Это мой сайт-визитк<span class="letter-highlight">а</span>
     </div>
   </hgroup>
-  <!-- Summary section -->
-  <section>
-    <h2>Кратко <span class="letter-highlight">о</span>бо мне</h2>
-    <div class="card-container">
-      <SummaryCard v-for="sum in summaries" :key="sum" :img="sum.img" :text="sum.text" />
-    </div>
-  </section>
-  <!-- Skills section -->
+  <SummarySection />
   <SkillsSection />
 </template>
-<script>
-import me from '../images/me.png'
-import me_and_who from '../images/me_and_who.png'
-const summaries = [
-  {
-    text: 'Cтудент первого курса по направлению программная инженерия ТУ им. А.А. Леонова.',
-    img: me
-  },
-  {
-    text: 'Занимаюсь каратэ Киокушинкай 7 лет. Так что могу и... на шпагат сесть.',
-    img: me_and_who
-  }
-]
-</script>
+<script></script>
