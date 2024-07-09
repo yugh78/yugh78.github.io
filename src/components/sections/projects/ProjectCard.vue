6d00f64a9295391619ff34e8facce0ec49ca9632
Katy248 <petrovanton247@gmail.com>
2024-07-09 19:41:09 +0300

    Add new component

---
+++ src/components/sections/projects/ProjectCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="project-card">
+    <div class="project-card-image" :style="{ 'background-image': `url('${project.image}')` }">
+      <div class="project-card-content">
+        <div class="project-card-body">
+          <div class="project-card-body-content">
+            <div class="project-card-body-text">
+              <div class="project-card-title">
+                {{ project.name }}
+              </div>
+              <div class="markup" v-html="project.description"></div>
+              <ProjectTagsContainer :tags="project.tags" />
+            </div>
+            <img
+              :src="project.gif"
+              class="h-40 min-w-32 bg-cover rounded-lg shadow-ziggurat-950 drop-shadow-sm border-ziggurat-400 border"
+              alt="Тут должна была быть гифка, подчёркивающая направление проекта"
+              loading="lazy"
+            />
+          </div>
+        </div>
+        <ProjectActions :actions="project.actions" />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import ProjectTagsContainer from '@/components/sections/projects/ProjectTags.vue'
+import ProjectActions from '@/components/sections/projects/ProjectActions.vue'
+defineProps({
+  project: {
+    image: String,
+    description: String,
+    gif: String,
+    name: String,
+    tags: [],
+    action: []
+  }
+})
+</script>
--- src/components/sections/projects/ProjectsSection.vue
+++ src/components/sections/projects/ProjectsSection.vue
@@ -3,30 +3,7 @@
     <h2>М<span class="letter-highlight">о</span>и проекты</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
       <div v-for="proj in PROJECTS" :key="proj.name" class="flex flex-col gap-2 justify-between">
-        <div class="project-card">
-          <div class="project-card-image" :style="{ 'background-image': `url('${proj.image}')` }">
-            <div class="project-card-content">
-              <div class="project-card-body">
-                <div class="project-card-body-content">
-                  <div class="project-card-body-text">
-                    <div class="project-card-title">
-                      {{ proj.name }}
-                    </div>
-                    <div class="markup" v-html="proj.description"></div>
-                    <ProjectTagsContainer :tags="proj.tags" />
-                  </div>
-                  <img
-                    :src="proj.gif"
-                    class="h-40 min-w-32 bg-cover rounded-lg shadow-ziggurat-950 drop-shadow-sm border-ziggurat-400 border"
-                    alt="Тут должна была быть гифка, подчёркивающая направление проекта"
-                    loading="lazy"
-                  />
-                </div>
-              </div>
-              <ProjectActions :actions="proj.actions" />
-            </div>
-          </div>
-        </div>
+        <ProjectCard :project="proj" />
       </div>
     </div>
   </section>
@@ -36,8 +13,7 @@ defineProps({
   sectionId: String
 })
 
-import ProjectTagsContainer from '@/components/sections/projects/ProjectTags.vue'
-import ProjectActions from '@/components/sections/projects/ProjectActions.vue'
+import ProjectCard from '@/components/sections/projects/ProjectCard.vue'
 
 import { PROJECTS } from '@/data/projects'
 </script>
