cadd91254c23155ddea27fb86bfb5c6dfc0eb652
Katy248 <petrovanton247@gmail.com>
2024-07-22 11:28:41 +0300

    Fix of fix

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,12 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        assetFileNames: 'asset',
-        chunkFileNames: 'chunk'
+        assetFileNames: (asset) => {
+          return 'assets/' + asset.name
+        },
+        chunkFileNames: (chInfo) => {
+          return 'chunks/chunk'
+        }
       }
     }
   }
