39d49d227b6a93964483a73957b99fbf3c99f9cb
Katy248 <petrovanton247@gmail.com>
2025-01-04 19:50:04 +0300

    Add tags sort

--- src/components/sections/projects/ProjectTags.vue
+++ src/components/sections/projects/ProjectTags.vue
@@ -1,26 +1,30 @@
 <template>
-  <div v-if="tags" class="project-tags-container">
-    <div v-for="tag in tags" :class="tagClasses(tag, selectedTags)" :key="tag"
-      @click="selectTag(selectedTags, tag) ? 'active' : ''">
-      <i :class="tag.icon"></i>
-      {{ tag.title }}
+    <div v-if="tags" class="project-tags-container">
+        <div v-for="tag in tags.sort(tagSort)" :class="tagClasses(tag, selectedTags)" :key="tag"
+            @click="selectTag(selectedTags, tag) ? 'active' : ''">
+            <i :class="tag.icon"></i>
+            {{ tag.title }}
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
 defineProps({ tags: [{ icon: String, title: String }], selectedTags: Array })
 
+const tagSort = (a, b) => {
+    return a.title[0] > b.title[0] ? 1 : -1;
+}
+
 const selectTag = (selectedTags, tag) => {
-  if (!selectedTags.includes(tag)) {
-    selectedTags.push(tag)
-  }
+    if (!selectedTags.includes(tag)) {
+        selectedTags.push(tag)
+    }
 }
 const tagClasses = (tag, selectedTags) => {
-  const baseClass = "project-tag"
-  if (selectedTags.includes(tag)) {
-    return baseClass + " active"
-  }
-  else { return baseClass }
+    const baseClass = "project-tag"
+    if (selectedTags.includes(tag)) {
+        return baseClass + " active"
+    }
+    else { return baseClass }
 }
 </script>
